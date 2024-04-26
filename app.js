76044de605f264182fecb580aa7548c6e8446b68
aeliilina <karybaevaaelina@gmail.com>
2024-04-26 18:08:19 +0600

    tasks

---
+++ app.js
@@ -0,0 +1,24 @@
+//создали массив
+// создали функцию
+// создали массив чтобы после фильтра в нем был только желамое число
+// условие чтобы item  должен быть больше 2
+// добавляем полученный результат в новый массив через push
+
+
+// var arr=[1, 2, 3, 1, 2]
+// function myFunc(filterArr){
+//     var arr=[]
+//     filterArr.forEach(function(item){
+//         if(item>2){
+//             arr.push(item)
+//         }
+//     })
+        
+//     return arr
+// }
+
+// console.log(myFunc(arr))
+
+
+
+
